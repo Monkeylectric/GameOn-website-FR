4dbccf23983c76af0f8d8b23f2a336eee9919c03
Monkeylectric <78590742+Monkeylectric@users.noreply.github.com>
2022-06-24 10:16:42 +0200

    Add files via upload

--- starterOnly/formValidation.js
+++ starterOnly/formValidation.js
@@ -17,164 +17,171 @@ let cityChecked = "";
 form.addEventListener("submit", validateForm);
 
 /** ------------------------------------------------------------
- * Firstname validation function
+ * Firstname validation function - checks if firstname form is valid
  * @param {Element} firstname 
  * @returns true or false
  */
 function firstnameValidation(firstname) {
-    const firstnameForm = firstname.parentElement;
+    const firstnameForm = firstname.closest('.formData');
     const nameRegex = /^[a-zA-z-éè]{2,15}$/;
 
-    if (!nameRegex.test(firstname.value) || firstname.value.length < 2 || firstname.value === "") {
-        if (firstname.value === "") {
-            firstnameForm.dataset.error = "Veuillez remplir le champ";
-        }else if (firstname.value.length < 2) {
-            firstnameForm.dataset.error = "Veuillez saisir au moins deux lettres";
-        }
-        else if (!nameRegex.test(firstname.value)) {
-            firstnameForm.dataset.error = "Veuillez n'utiliser que des lettres";
-        }
+    if (firstname.value.trim() === "") {
+        firstnameForm.dataset.error = "Veuillez remplir le champ";
         firstnameForm.dataset.errorVisible = "true";
         return false;
-    }else {
-        firstnameForm.dataset.errorVisible = "false";
-        return true;
     }
+    if (firstname.value.length < 2) {
+        firstnameForm.dataset.error = "Veuillez saisir au moins deux lettres";
+        firstnameForm.dataset.errorVisible = "true";
+        return false;
+    }
+    if (!nameRegex.test(firstname.value)) {
+        firstnameForm.dataset.error = "Veuillez n'utiliser que des lettres";
+        firstnameForm.dataset.errorVisible = "true";
+        return false;
+    }
+    
+    firstnameForm.dataset.errorVisible = "false";
+    return true;
 }
 
 /** ------------------------------------------------------------
- * Lastname validation function
+ * Lastname validation function - checks if lastname form is valid
  * @param {Element} lastname 
  * @returns true or false
  */
 function lastnameValidation(lastname) {
-    const lastnameForm = lastname.parentElement;
+    const lastnameForm = lastname.closest('.formData');
     const nameRegex = /^[a-zA-z-éè]{2,15}$/;
 
-    if (!nameRegex.test(lastname.value) || lastname.value.length < 2 || lastname.value === "") {
-        if (lastname.value === "") {
-            lastnameForm.dataset.error = "Veuillez remplir le champ";
-        }else if (lastname.value.length < 2) {
-            lastnameForm.dataset.error = "Veuillez saisir au moins deux lettres";
-        }else if (!nameRegex.test(lastname.value)) {
-            lastnameForm.dataset.error = "Veuillez n'utiliser que des lettres";
-        }
+    if (lastname.value.trim() === "") {
+        lastnameForm.dataset.error = "Veuillez remplir le champ";
         lastnameForm.dataset.errorVisible = "true";
         return false;
-    }else {
-        lastnameForm.dataset.errorVisible = "false";
-        return true;
     }
+    if (lastname.value.length < 2) {
+        lastnameForm.dataset.error = "Veuillez saisir au moins deux lettres";
+        lastnameForm.dataset.errorVisible = "true";
+        return false;
+    }
+    if (!nameRegex.test(lastname.value)) {
+        lastnameForm.dataset.error = "Veuillez n'utiliser que des lettres";
+        lastnameForm.dataset.errorVisible = "true";
+        return false;
+    }
+    
+    lastnameForm.dataset.errorVisible = "false";
+    return true;
 }
 
 /** ------------------------------------------------------------
- * Email validation function
+ * Email validation function - checks if email form is valid
  * @param {Element} email 
  * @returns true or false
  */
 function mailValidation(email) {
-    const emailForm = email.parentElement;
+    const emailForm = email.closest('.formData');
     const mailRegex = /^[A-Za-z0-9](([_\.\-]?[a-zA-Z0-9]+)*)@([A-Za-z0-9]+)(([_\.\-]?[a-zA-Z0-9]+)*)\.([A-Za-z]{2,})$/;
 
-    if (!mailRegex.test(email.value) || email.value === "") {
-        if (email.value === "") {
-            emailForm.dataset.error = "Veuillez remplir le champ";
-        }else if (!mailRegex.test(email.value)) {
-            emailForm.dataset.error = "Veuillez renseigner une adresse mail valide";
-        }
+    if (email.value.trim() === "") {
+        emailForm.dataset.error = "Veuillez remplir le champ";
         emailForm.dataset.errorVisible = "true";
         return false;
-    }else {
-        emailForm.dataset.errorVisible = "false";
-        return true;
     }
+    if (!mailRegex.test(email.value)) {
+        emailForm.dataset.error = "Veuillez renseigner une adresse mail valide";
+        emailForm.dataset.errorVisible = "true";
+        return false;
+    }
+
+    emailForm.dataset.errorVisible = "false";
+    return true;
 }
 
 /** ------------------------------------------------------------
- * Birthdate validation function
+ * Birthdate validation function - checks if birthdate form is valid
  * @param {Element} birthdate 
  * @returns true or false
  */
 function birthdateValidation(birthdate) {
-    const birthdateForm = birthdate.parentElement;
+    const birthdateForm = birthdate.closest('.formData');
     const birthdayRegex = /^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$/;
 
-    if (!birthdayRegex.test(birthdate.value) || birthdate.value === "") {
-        if (birthdate.value === "") {
-            birthdateForm.dataset.error = "Veuillez remplir le champ";
-        }else if (!birthdayRegex.test(birthdate.value)) {
-            birthdateForm.dataset.error = "Veuillez rentrer une date valide";
-        }
+    if (birthdate.value.trim() === "") {
+        birthdateForm.dataset.error = "Veuillez remplir le champ";
         birthdateForm.dataset.errorVisible = "true";
-
         return false;
-    }else {
-        birthdateForm.dataset.errorVisible = "false";
-        return true;
     }
+    if (!birthdayRegex.test(birthdate.value)) {
+        birthdateForm.dataset.error = "Veuillez rentrer une date valide";
+        birthdateForm.dataset.errorVisible = "true";
+        return false;
+    }
+
+    birthdateForm.dataset.errorVisible = "false";
+    return true;
 }
 
 /** ------------------------------------------------------------
- * Quantity validation function
+ * Quantity validation function - checks if quantity form is valid
  * @param {Element} quantity 
  * @returns true or false
  */
 function quantityValidation(quantity) {
-    const quantityForm = quantity.parentElement;
-    const quantityRegex = /^[0-9][0-9]?$/;
+    const quantityForm = quantity.closest('.formData');
+    const quantityRegex = /^\d{1,2}$/;
 
-    if (!quantityRegex.test(quantity.value) || quantity.value === "") {
-        if (quantity.value === "") {
-            quantityForm.dataset.error = "Veuillez remplir le champ";
-        }else if (!quantityRegex.test(quantity.value)) {
-            quantityForm.dataset.error = "Le nombre doit être compris entre 0 et 99";
-        }
+    if (quantity.value.trim() === "") {
+        quantityForm.dataset.error = "Veuillez remplir le champ";
         quantityForm.dataset.errorVisible = "true";
         return false;
-    }else {
-        quantityForm.dataset.errorVisible = "false";
-        return true;
     }
+    if (!quantityRegex.test(quantity.value)) {
+        quantityForm.dataset.error = "Le nombre doit être compris entre 0 et 99";
+        quantityForm.dataset.errorVisible = "true";
+        return false;
+    }
+
+    quantityForm.dataset.errorVisible = "false";
+    return true;
 }
 
 /** ------------------------------------------------------------
- * City validation function
+ * City validation function - checks if city form is valid
  * @param {NodeElements} cityForm 
  * @returns true or false
  */
-function locationValidation(cityForm) {
-    cityForm.forEach(city => {
-        if(city.checked){
-            cityChecked = city.value;
-            city.parentElement.dataset.errorVisible = "false";
-        }else {
-            city.parentElement.dataset.error = "Veuillez selectionné une ville";
-            city.parentElement.dataset.errorVisible = "true";
+function locationValidation(cities) {
+    for (let i = 0; i < cities.length; i++) {
+        if(cities[i].checked){
+            cityChecked = cities[i].value;
+            cities[i].closest('.formData').dataset.errorVisible = "false";
+            return true;
         }
-    });
 
-    if (cityChecked != "") {
-        return true;
+        cities[i].closest('.formData').dataset.error = "Veuillez selectionné une ville";
+        cities[i].closest('.formData').dataset.errorVisible = "true";
     }
 }
 
 /** ------------------------------------------------------------
- * Checkbox validation function
+ * Checkbox validation function - checks if checkbox form is valid
  * @param {Element} checkbox 
  * @returns true or false
  */
 function checkboxValidation(checkbox) {
     if (checkbox.checked) {
-        checkbox.parentElement.dataset.errorVisible = "false";
+        checkbox.closest('.formData').dataset.errorVisible = "false";
         return true;
     }
-        checkbox.parentElement.dataset.error = "Veuillez accepter les conditions d'utilisation";
-        checkbox.parentElement.dataset.errorVisible = "true";
-        return false;
+
+    checkbox.closest('.formData').dataset.error = "Veuillez accepter les conditions d'utilisation";
+    checkbox.closest('.formData').dataset.errorVisible = "true";
+    return false;
 }
 
 /** ------------------------------------------------------------
- * Validate form function
+ * Validate form function - checks if every forms are valid
  * @param {Object} e 
  * @returns true or false
  */
@@ -191,6 +198,8 @@ function checkboxValidation(checkbox) {
         locationValidation(cityForm),
         checkboxValidation(checkbox1)
     ];
+
+    console.log(verification);
 
     //-- If every data are correct
     if(verification.every(Boolean)) {
